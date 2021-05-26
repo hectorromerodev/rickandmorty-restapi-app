2bf47cea7425829860fac8546b2ddc29a23d24f5
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 11:46:24 -0700

    UPD app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { SpinnerModule } from '@components/spinner/spinner.module';
 import { HeaderModule } from '@components/header/header.module';
 import { FooterModule } from '@components/footer/footer.module';
 import { SearchModule } from '@components/search/search.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ToastrModule } from 'ngx-toastr';
-import { SpinnerInterceptor } from './shared/interceptors/spinner.interceptor';
+import { SpinnerInterceptor } from '@shared/interceptors/spinner.interceptor';
+import { ScrollUpDirective } from '@shared/directives/scroll-up/scroll-up.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,11 +21,13 @@ import { SpinnerInterceptor } from './shared/interceptors/spinner.interceptor';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot(), // ToastrModule added
+    ToastrModule.forRoot({
+      preventDuplicates: true
+    }), // ToastrModule added
     SpinnerModule,
     HeaderModule,
     FooterModule,
-    SearchModule
+    SearchModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true }
