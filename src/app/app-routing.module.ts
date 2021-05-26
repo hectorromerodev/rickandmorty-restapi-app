a352e4e2df73285519107c88bd18ad3615962c9d
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 17:04:37 -0700

    UPD App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'characters-list', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'episodes', loadChildren: () => import('./pages/episodes/episodes.module').then(m => m.EpisodesModule) },
-  { path: 'character-list', loadChildren: () => import('./pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
+  { path: 'characters-list', loadChildren: () => import('./pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
   { path: 'character-details/:id', loadChildren: () => import('./pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) },
   { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
   { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
