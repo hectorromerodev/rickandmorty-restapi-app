e308b6422aa3403597842e2231222583e63b475c
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 08:51:45 -0700

    UPD Spinner component

--- src/app/shared/components/spinner/spinner.component.ts
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '@shared/services/spinner.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-spinner',
-  templateUrl: './spinner.component.html',
+  template: `
+    <div class="overlay" *ngIf="isLoading$ | async">
+      <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+    </div>
+  `,
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SpinnerComponent {
+  isLoading$: Subject<boolean> = this.spinnerServ.isLoading$;
+  constructor(private spinnerServ: SpinnerService) { }
 }
