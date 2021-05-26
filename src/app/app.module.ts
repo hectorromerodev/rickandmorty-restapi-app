5bdf9c3c8a0d1dc34f3b0a9d2754c0af7b09cef3
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 17:04:53 -0700

    UPD App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
-import { SearchComponent } from './shared/components/search/search.component';
 import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent,
     SpinnerComponent,
-    NotFoundComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
