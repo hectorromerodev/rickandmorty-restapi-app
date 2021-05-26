efb2ec6faa9beed0e44bede2875da29fc1957920
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 20:29:37 -0700

    UPD data servicw

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Character } from '@interfaces/character.interface';
+import { Episode } from '@interfaces/episode.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private episodesSubject = new BehaviorSubject<Episode[]>([]);
+  episodes$ = this.episodesSubject.asObservable();
+  private charactersSubject = new BehaviorSubject<Character[]>([]);
+  characters$ = this.charactersSubject.asObservable();
 
   constructor() { }
+
+
+
+
 }
