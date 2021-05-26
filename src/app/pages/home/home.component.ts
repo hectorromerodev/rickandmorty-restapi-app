ce710782275df71725ba0b277bff5c61f464423f
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 20:28:14 -0700

    UPD home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,14 +4,14 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   template: `
     <h1 class="title">My favorites characters</h1>
-    <section *ngIf="charactersFav$ | async as characters" class="character__list">
+    <!-- <section *ngIf="charactersFav$ | async as characters" class="character__list">
       <ng-container *ngIf="characters.length; else noFavorites">
         <app-characters-card *ngFor="let character of characters" [character]="character"></app-characters-card>
       </ng-container>
       <ng-template #noFavorites>
         <app-not-data title="" message="You don't have any favorites yet"></app-not-data>
       </ng-template>
-    </section>
+    </section> -->
   `,
   styles: [`
     .character__list{
@@ -22,10 +22,9 @@ import { Component, OnInit } from '@angular/core';
   `]
 })
 export class HomeComponent implements OnInit {
-
+  charactersFav$: any;
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
