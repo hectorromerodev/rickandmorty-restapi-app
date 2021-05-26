3ec77fa1af6a598b7af2b21481287b0a0ad90a73
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 20:29:46 -0700

    UPD app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { NotDataComponent } from './shared/components/not-found/not-data.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NotDataModule } from './shared/components/not-data/not-data.module';
+import { SpinnerModule } from './shared/components/spinner/spinner.module';
+import { HeaderModule } from './shared/components/header/header.module';
+import { FooterModule } from './shared/components/footer/footer.module';
+import { SearchModule } from './shared/components/search/search.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    SpinnerComponent,
-    FooterComponent,
-    NotDataComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  exports: [
-    NotDataComponent
+    AppRoutingModule,
+    HttpClientModule,
+    NotDataModule,
+    SpinnerModule,
+    HeaderModule,
+    FooterModule,
+    SearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
