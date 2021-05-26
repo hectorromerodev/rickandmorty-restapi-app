a26319e2d72adad1c7b2955c541b49393c8a85b1
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 08:51:27 -0700

    UPD spinner interceptor

--- src/app/shared/interceptors/spinner.interceptor.ts
+++ src/app/shared/interceptors/spinner.interceptor.ts
@@ -6,13 +6,22 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { SpinnerService } from '../services/spinner.service';
+import { finalize } from 'rxjs/operators';
 
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private spinnerServ: SpinnerService
+  ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    this.spinnerServ.show();
+    return next
+      .handle(request)
+      .pipe(
+        finalize(() => this.spinnerServ.hide())
+      );
   }
 }
