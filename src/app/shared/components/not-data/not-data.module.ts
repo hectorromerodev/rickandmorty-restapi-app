30f05624ea462eb6654c65445ae5424380870a1e
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 20:23:33 -0700

    ADD not-data module

---
+++ src/app/shared/components/not-data/not-data.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotDataComponent } from './not-data.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotDataComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NotDataComponent
+  ]
+})
+export class NotDataModule { }
