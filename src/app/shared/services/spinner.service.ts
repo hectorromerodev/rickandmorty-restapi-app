6c5113ad61a11f692b1d88af5397f73019a21e4e
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 08:49:34 -0700

    UPD Spinner service

--- src/app/shared/services/spinner.service.ts
+++ src/app/shared/services/spinner.service.ts
@@ -5,7 +5,7 @@ import { Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class SpinnerService {
-  isLoading$ = new Subject();
+  isLoading$: Subject<boolean> = new Subject();
 
   show(): void {
     this.isLoading$.next(true);
