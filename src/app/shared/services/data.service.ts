5d8b4f7b5939ba337efe10dcb3e32e57f044995c
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 00:17:32 -0700

    UPD data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Character } from '@interfaces/character.interface';
 import { Episode } from '@interfaces/episode.interface';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '@environment/environment';
+import { pluck, take, tap, withLatestFrom } from 'rxjs/operators'
+
+const ENDPOINT: string = `${environment.BASE_URL}/`;
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +17,34 @@ export class DataService {
   private charactersSubject = new BehaviorSubject<Character[]>([]);
   characters$ = this.charactersSubject.asObservable();
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.getEpisodes();
+    this.getCharacters();
+  }
 
+  getEpisodes(pageNum = 1) {
+    const params = new HttpParams()
+      .set('page', pageNum);
+    this.http.get<any>(ENDPOINT.concat(environment.EPISODES), { params })
+      .pipe(
+        take(1),
+        pluck('results'),
+        withLatestFrom(this.episodes$),
+        tap(([apiResponse, episodes]) => this.episodesSubject.next([...apiResponse, ...episodes]))
+      ).subscribe();
+  }
 
+  getCharacters(pageNum = 1) {
+    const params = new HttpParams()
+      .set('page', pageNum);
+    this.http.get<any>(ENDPOINT.concat(environment.CHARACTERS), { params })
+      .pipe(
+        take(1),
+        pluck('results'),
+        withLatestFrom(this.characters$),
+        tap(([apiResponse, characters]) => this.charactersSubject.next([...apiResponse, ...characters]))
+      ).subscribe();
+  }
 
 
 }
