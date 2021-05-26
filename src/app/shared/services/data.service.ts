2c0e67a8a63f3b0e684431cceace8e11ad8387b0
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 11:45:56 -0700

    UPD data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, pluck, take, tap, throttleTime, withLatestFrom } from 'rxjs/operators'
 import { BehaviorSubject, Observable, of } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+
 import { Character } from '@interfaces/character.interface';
 import { Episode } from '@interfaces/episode.interface';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '@environment/environment';
-import { catchError, pluck, take, tap, withLatestFrom } from 'rxjs/operators'
-import { ToastrService } from 'ngx-toastr';
 
 const ENDPOINT: string = `${environment.BASE_URL}/`;
 
@@ -26,33 +27,34 @@ export class DataService {
     this.getCharacters();
   }
 
-  getEpisodes(pageNum = 1) {
-    const params = new HttpParams()
-      .set('page', pageNum);
+  getEpisodes(pageNum = 1): void {
+    const params = new HttpParams().set('page', pageNum);
     this.http.get<any>(ENDPOINT.concat(environment.EPISODES), { params })
       .pipe(
         take(1),
         pluck('results'),
         withLatestFrom(this.episodes$),
-        tap(([apiResponse, episodes]) => this.episodesSubject.next([...apiResponse, ...episodes])),
+        tap(([responseApi, episodes]) => this.episodesSubject.next([...episodes, ...responseApi])),
         catchError(error => {
           return this.showToastr(error, 'No more episodes to load');
         })
       ).subscribe();
   }
 
-  getCharacters(pageNum = 1) {
-    const params = new HttpParams()
-      .set('page', pageNum);
+  getCharacters(pageNum = 1): void {
+    const params = new HttpParams().set('page', pageNum);
     this.http.get<any>(ENDPOINT.concat(environment.CHARACTERS), { params })
       .pipe(
         take(1),
         pluck('results'),
         withLatestFrom(this.characters$),
-        tap(([apiResponse, characters]) => this.charactersSubject.next([...apiResponse, ...characters])),
+        tap(([responseApi, characters]) => {
+          this.charactersSubject.next([...characters, ...responseApi]);
+        }),
+        throttleTime(1000),
         catchError(error => {
           return this.showToastr(error, 'No more characters to load');
-        })
+        }),
       ).subscribe();
   }
 
@@ -60,8 +62,9 @@ export class DataService {
     if (error.status === 404) {
       this.toastr.info(message);
     } else {
-      this.toastr.error(error)
+      console.log(error)
+      this.toastr.error(error.message)
     }
-    return of(error);
+    return of([]);
   }
 }
