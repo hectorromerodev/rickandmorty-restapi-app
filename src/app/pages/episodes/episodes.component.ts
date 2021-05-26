ce27090af3abbbf6b490d820bca837c8da38b3f2
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 02:24:20 -0700

    UPD Episodes page

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, HostListener, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Episode } from '@shared/interfaces/episode.interface';
 import { DataService } from '@services/data.service';
 import { Observable } from 'rxjs';
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
   selector: 'app-episodes',
   template: `
     <h1 class="title">Episodes</h1>
-    <section class="container">
+    <section class="container" scrollUp (showBtn)="showScroll = $event">
       <ul class="episodes__list" *ngIf="episodes$ | async as episodes">
         <ng-container *ngIf="episodes.length; else noData">
           <li *ngFor="let episode of episodes">
@@ -20,59 +20,15 @@ import { Observable } from 'rxjs';
         </ng-template>
       </ul>
       <!-- SCROLL UP -->
-      <figure *ngIf="showScrollBtn" class="scroll">
-        <img (click)="onScrollUp()" src="assets/imgs/scroll_up.svg" alt="Scroll up" rel="preload" as="image" width="45" height="45">
-      </figure>
+      <app-scroll-up [showScrollBtn]="showScroll"></app-scroll-up>
     </section>
-  `,
-  styles: [`
-    .scroll{
-      position: fixed;
-      bottom: 20px;
-      right: 20px;
-      z-index: 3;
-      padding: 1rem;
-      border-radius: 50%;
-      font-size: 1.5rem;
-      height: 45px;
-      width: 45px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      &:hover{
-        cursor:pointer;
-        transform: scale(1.2);
-        background-color: #f1f1f154;
-        transition: all .6s ease-in-out;
-      }
-    }
-  `]
+  `
 })
 export class EpisodesComponent {
   episodes$: Observable<Episode[]> = this.dataServ.episodes$;
 
-  showScrollBtn: boolean = false;
-  private scrollHeight = 500;
+  showScroll: boolean = false;
   private pageNum = 1;
 
-  constructor(
-    private dataServ: DataService,
-    @Inject(DOCUMENT) private document: Document
-  ) { }
-
-  @HostListener('window:scroll')
-  onWindowScroll(): void {
-    const yOffset = window.pageYOffset;
-    const scrollTop = this.document.documentElement.scrollTop;
-    this.showScrollBtn = (yOffset || scrollTop) > this.scrollHeight;
-  }
-
-  onScrollUp(): void {
-    this.document.documentElement.scrollTop = 0;
-  }
-
-  onScrollDown(): void {
-    this.pageNum++;
-    this.dataServ.getEpisodes(this.pageNum);
-  }
+  constructor(private dataServ: DataService) { }
 }
--- src/app/pages/episodes/episodes.module.ts
+++ src/app/pages/episodes/episodes.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { EpisodesRoutingModule } from './episodes-routing.module';
 import { EpisodesComponent } from './episodes.component';
 import { NotDataModule } from '@app/shared/components/not-data/not-data.module';
+import { scrollUpDirective } from '@app/shared/directives/scroll-up.directive';
+import { ScrollUpModule } from '@app/shared/components/scroll-up/scroll-up.module';
 
 
 @NgModule({
   declarations: [
-    EpisodesComponent
+    EpisodesComponent,
+    scrollUpDirective
   ],
   imports: [
     CommonModule,
     EpisodesRoutingModule,
-    NotDataModule
+    NotDataModule,
+    ScrollUpModule
   ]
 })
 export class EpisodesModule { }
