7c795f660b24a7dd3061b7c07918bc5d23fee177
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 00:17:42 -0700

    DEL unused interfaces

--- src/app/shared/interfaces/data-response.interface.ts
+++
@@ -1,8 +0,0 @@
-import { APIResponse } from "./api-response.interface";
-import { Character } from "./character.interface";
-import { Episode } from "./episode.interface";
-
-export interface DataResponse {
-  characters: APIResponse<Character[]>;
-  episodes: APIResponse<Episode[]>;
-}
