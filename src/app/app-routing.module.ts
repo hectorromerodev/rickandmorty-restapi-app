8aa3a50166ee8e22a461db3563bbdf6de5fe0cd7
Hector Romero <hectorromerodev@gmail.com>
2021-05-25 19:56:50 -0700

    UPD App routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'characters-list', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'episodes', loadChildren: () => import('./pages/episodes/episodes.module').then(m => m.EpisodesModule) },
-  { path: 'characters-list', loadChildren: () => import('./pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
-  { path: 'character-details/:id', loadChildren: () => import('./pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) },
-  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
-  { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
-
+  { path: 'home', loadChildren: () => import('@pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'episodes', loadChildren: () => import('@pages/episodes/episodes.module').then(m => m.EpisodesModule) },
+  { path: 'characters-list', loadChildren: () => import('@characters/characters-list/characters-list.module').then(m => m.CharactersListModule) },
+  { path: 'character-details/:id', loadChildren: () => import('@characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) },
+  { path: 'about', loadChildren: () => import('@pages/about/about.module').then(m => m.AboutModule) },
+  { path: '**', loadChildren: () => import('@pages/not-found/not-found.module').then(m => m.NotFoundModule) },
 ];
 
 @NgModule({
