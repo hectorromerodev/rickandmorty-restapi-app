e6ac23892a97dece0a22ddeabecc1d0f3293ee36
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 17:08:01 -0700

    UPD data service

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -7,6 +7,7 @@ import { ToastrService } from 'ngx-toastr';
 import { Character } from '@interfaces/character.interface';
 import { Episode } from '@interfaces/episode.interface';
 import { environment } from '@environment/environment';
+import { LocalStorageService } from './local-storage.service';
 
 const ENDPOINT: string = `${environment.BASE_URL}/`;
 
@@ -16,12 +17,17 @@ const ENDPOINT: string = `${environment.BASE_URL}/`;
 export class DataService {
   private episodesSubject = new BehaviorSubject<Episode[]>([]);
   episodes$ = this.episodesSubject.asObservable();
+
   private charactersSubject = new BehaviorSubject<Character[]>([]);
   characters$ = this.charactersSubject.asObservable();
 
+  private oneCharacterSubject = new BehaviorSubject<Character>({} as Character);
+  oneCharacter$ = this.oneCharacterSubject.asObservable();
+
   constructor(
     private http: HttpClient,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private localServ: LocalStorageService
   ) {
     this.getEpisodes();
     this.getCharacters();
@@ -49,13 +55,47 @@ export class DataService {
         pluck('results'),
         withLatestFrom(this.characters$),
         tap(([responseApi, characters]) => {
-          this.charactersSubject.next([...characters, ...responseApi]);
+          this.parseCharactersData([...characters, ...responseApi]);
         }),
         throttleTime(1000),
         catchError(error => {
           return this.showToastr(error, 'No more characters to load');
         }),
       ).subscribe();
+  }
+
+  filterCharacters(valueToSearch: string): void {
+    const params = new HttpParams().set('name', valueToSearch);
+    this.http.get<any>(ENDPOINT.concat(environment.CHARACTERS), { params })
+      .pipe(
+        take(1),
+        pluck('results'),
+        tap((apiResponse) => this.parseCharactersData(apiResponse)),
+        catchError(error => {
+          console.warn(error.message);
+          this.charactersSubject.next([]);
+          return of(error);
+        })
+      ).subscribe();
+  }
+
+  getDetails(id: number): Observable<Character> {
+    console.log(`${ENDPOINT}${environment.CHARACTERS}/${id}`)
+    this.http.get<any>(`${ENDPOINT}${environment.CHARACTERS}/${id}`)
+      .pipe(
+        take(1),
+        tap((character: Character) => this.oneCharacterSubject.next(character))
+      ).subscribe();
+    return this.oneCharacter$;
+  }
+
+  private parseCharactersData(characters: Character[]): void {
+    const currentFavs: Character[] = this.localServ.getFavoritesChar();
+    const newData = characters.map((character: Character) => {
+      const found = !!currentFavs.find((fav: Character) => fav.id === character.id);
+      return { ...character, isFavorite: found }
+    });
+    this.charactersSubject.next(newData);
   }
 
   showToastr(error: any, message: string): Observable<any> {
