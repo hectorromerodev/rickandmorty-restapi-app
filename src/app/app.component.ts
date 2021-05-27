188c76ac04fb9515ea31f3bd89a7319be5d33a86
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 17:19:36 -0700

    UPD app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <app-spinner></app-spinner>
+    <app-header></app-header>
+    <main class="container">
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  `,
 })
 export class AppComponent { }
