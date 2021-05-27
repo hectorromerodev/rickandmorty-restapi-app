6f9f6e0c37010ecc67ba5facd28899cc2a3db557
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 17:07:15 -0700

    UPD episodes page

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Episode } from '@interfaces/episode.interface';
 import { DataService } from '@services/data.service';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-episodes',
-  templateUrl: './episodes.component.html'
+  templateUrl: './episodes.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EpisodesComponent {
   episodes$: Observable<Episode[]> = this.dataServ.episodes$;
