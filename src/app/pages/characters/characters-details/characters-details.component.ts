f6e32d95a77e5c4fad571f3c8ba7f2729f65dc35
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 17:07:06 -0700

    UPD Characters details page

--- src/app/pages/characters/characters-details/characters-details.component.ts
+++ src/app/pages/characters/characters-details/characters-details.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { take, tap } from 'rxjs/operators';
 
+import { DataService } from '@services/data.service';
+import { Character } from '@interfaces/character.interface';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-characters-details',
-  templateUrl: './characters-details.component.html',
-  styleUrls: ['./characters-details.component.scss']
+  template: `
+    <section class="character__details">
+      <app-characters-card *ngIf="character$ | async as character" [character]="character" ></app-characters-card>
+    </section>
+  `,
+  styles: [`
+    .character__details{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+    }
+  `]
 })
-export class CharactersDetailsComponent implements OnInit {
+export class CharactersDetailsComponent {
+  characterId!: any;
+  character$!: Observable<Character>;
+  constructor(
+    private dataServ: DataService,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.pipe(
+      take(1),
+      tap(({ id }) => this.character$ = this.dataServ.getDetails(id))
+    ).subscribe();
 
-  constructor() { }
 
-  ngOnInit(): void {
   }
-
 }
