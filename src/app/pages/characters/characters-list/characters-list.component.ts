a85b1e6e73f0f37e23c1aa1cff6888071087ec4e
Hector Romero <hectorromerodev@gmail.com>
2021-05-26 17:06:40 -0700

    UPD characters list page

--- src/app/pages/characters/characters-list/characters-list.component.ts
+++ src/app/pages/characters/characters-list/characters-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Character } from '@interfaces/character.interface';
+import { DataService } from '@services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-characters-list',
   templateUrl: './characters-list.component.html',
   styleUrls: ['./characters-list.component.scss']
 })
-export class CharactersListComponent implements OnInit {
+export class CharactersListComponent {
+  characters$: Observable<Character[]> = this.dataServ.characters$;
+  showScroll: boolean = false;
+  private pageNum: number = 1;
 
-  constructor() { }
+  constructor(private dataServ: DataService) { }
 
-  ngOnInit(): void {
+  onScrollDown(): void {
+    this.pageNum++;
+    this.dataServ.getCharacters(this.pageNum);
   }
-
 }
--- src/app/pages/characters/characters-list/characters-list.module.ts
+++ src/app/pages/characters/characters-list/characters-list.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { CharactersListRoutingModule } from './characters-list-routing.module';
 import { CharactersListComponent } from './characters-list.component';
+import { CharactersCardModule } from '@characters/characters-card/characters-card.module';
+import { NotDataModule } from '@components/not-data/not-data.module';
+import { SearchModule } from '@components/search/search.module';
+import { ScrollUpModule } from '@components/scroll-up/scroll-up.module';
+import { ScrollUpDirectiveModule } from '@shared/directives/scroll-up/scroll-up-directive.module';
 
 
 @NgModule({
@@ -11,7 +17,13 @@ import { CharactersListComponent } from './characters-list.component';
   ],
   imports: [
     CommonModule,
-    CharactersListRoutingModule
+    CharactersListRoutingModule,
+    CharactersCardModule,
+    SearchModule,
+    NotDataModule,
+    ScrollUpModule,
+    InfiniteScrollModule,
+    ScrollUpDirectiveModule
   ]
 })
 export class CharactersListModule { }
